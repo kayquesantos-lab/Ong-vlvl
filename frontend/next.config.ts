636153c9f6b9e515a92ce9cb715f4c0292b6d669
kayquesantos-lab <kayquesantos@ucl.br>
2026-06-01 22:04:14 -0300

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http',  hostname: 'localhost' },
+    ],
+  },
+  serverExternalPackages: ['js-cookie'],
+}
 
 export default nextConfig
