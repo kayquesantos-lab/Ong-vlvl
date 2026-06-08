7a31827758a59a27eb7310452cde7abcb211fd16
Igor Rangel de Souza <163342979+Igorrangelsouza@users.noreply.github.com>
2026-06-08 18:03:06 -0300

    Test deploy middleware.ts

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,10 +1,1 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
 
-export function middleware(req: NextRequest) {
-  return NextResponse.next()
-}
-
-export const config = {
-  matcher: ['/login'],
-}
